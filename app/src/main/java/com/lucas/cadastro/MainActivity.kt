be162daeb5390e5ce59be686621cf3bd9be49739
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-02-22 00:52:18 -0300

    feat: improve style and handle erros on form  - change primary color  - change font family  - change edit text element style  - handle blank answers

--- app/src/main/java/com/lucas/cadastro/MainActivity.kt
+++ app/src/main/java/com/lucas/cadastro/MainActivity.kt
@@ -23,16 +23,43 @@ class MainActivity : AppCompatActivity() {
         with(activityMainBinding) {
             buttonSalvar.setOnClickListener {
                 val fullName = inputName.text.toString()
+                if (fullName.isBlank()) {
+                    Toast.makeText(this@MainActivity, "O nome não pode ser vazio", Toast.LENGTH_LONG).show()
+                    return@setOnClickListener
+                }
+
                 val phone = inputPhone.text.toString()
+                if (phone.isBlank()) {
+                    Toast.makeText(this@MainActivity, "O telefone não pode ser vazio", Toast.LENGTH_LONG).show()
+                    return@setOnClickListener
+                }
+
                 val email = inputEmail.text.toString()
+                if (email.isBlank()) {
+                    Toast.makeText(this@MainActivity, "O email não pode ser vazio", Toast.LENGTH_LONG).show()
+                    return@setOnClickListener
+                }
+
                 val isOnEmailList = checkboxEmail.isChecked
+
                 val gender = when (radioGroup.checkedRadioButtonId) {
                     radioButtonMasculino.id -> "Masculino"
                     radioButtonFeminino.id -> "Feminino"
                     else -> ""
                 }
+                if (gender == "" ) {
+                    Toast.makeText(this@MainActivity, "O gênero não pode ser vazio", Toast.LENGTH_LONG).show()
+                    return@setOnClickListener
+                }
+
                 val city = inputCidade.text.toString()
+                if (city.isBlank()) {
+                    Toast.makeText(this@MainActivity, "A cidade não pode ser vazia", Toast.LENGTH_LONG).show()
+                    return@setOnClickListener
+                }
+
                 val uf = spinner.selectedItem.toString()
+
                 val formulario = Formulario(fullName, phone, email, isOnEmailList, gender, city, uf)
 
                 Toast.makeText(this@MainActivity, formulario.toString(), Toast.LENGTH_LONG).show()
