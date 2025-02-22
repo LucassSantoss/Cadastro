153beff6ee0798e805af96578e8920a70ef3023c
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-02-21 23:58:33 -0300

    start to use activityMainBinding

--- app/src/main/java/com/lucas/cadastro/MainActivity.kt
+++ app/src/main/java/com/lucas/cadastro/MainActivity.kt
@@ -5,10 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.lucas.cadastro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activityMainBinding.root)
+
+        with(activityMainBinding) {
+
+        }
     }
 }
